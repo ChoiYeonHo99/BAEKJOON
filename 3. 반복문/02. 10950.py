011e2cd53b69a6f1cb7c1cb61fb5720b443643d8
ChoiYeonHo99 <70682926+ChoiYeonHo99@users.noreply.github.com>
2023-02-22 13:11:00 +0900

    Delete 3. 반복문 directory

--- 3. 반복문/02. 10950.py
+++
@@ -1,7 +0,0 @@
-x = int(input())
-
-for i in range(0, x):
-	s = input().split(" ")
-	a = int(s[0])
-	b = int(s[1])
-	print(a + b)
--- 3. 반복문/05. 15552.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-n = int(sys.stdin.readline().rstrip())
-
-for i in range(0, n):
-	s = sys.stdin.readline().rstrip().split()
-	A = int(s[0])
-	B = int(s[1])
-	print(A + B)
